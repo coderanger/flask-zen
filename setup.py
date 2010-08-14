07767b1ec82fd26022bbb9af828f014a480404ca
Noah Kantrowitz <noah@coderanger.net>
2010-08-14 16:42:37 -0700

    Initial commit of the split Flask extension for PyZen.

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+import os
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Flask-Zen',
+    version = '0.1',
+    packages = find_packages(),
+    author = 'Noah Kantrowitz',
+    author_email = 'noah@coderanger.net',
+    description = 'Flask-Script commands to integrate with PyZen.',
+    long_description = open(os.path.join(os.path.dirname(__file__), 'README')).read(),
+    license = 'BSD',
+    keywords = 'test unittest continuous flask',
+    url = 'http://github.com/coderanger/flask-zen',
+    classifiers = [
+        #'Development Status :: 1 - Planning',
+        #'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
+        #'Development Status :: 4 - Beta',
+        #'Development Status :: 5 - Production/Stable',
+        #'Development Status :: 6 - Mature',
+        #'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Environment :: Win32 (MS Windows)',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    zip_safe = False,
+)
